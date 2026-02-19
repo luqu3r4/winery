1462ffd411dc9e493592efd34ab675efea255508
Ruben <ruben.laczor@educa.jcyl.es>
2026-02-19 14:07:23 +0100

    Añadido clase DepositType

---
+++ models/deposit_type.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class DepositType(models.Model):
+    _name = 'deposit.type'
+    _description = 'Tipo de Deposito'
+
+    name = fields.Char(string='Name', required = True)
+    code = fields.Char(string='Internal Code', required=True)
+
+    material = fields.Char(string='Material', required=True)
+    default_capacity = fields.Float(
+        string='Default Capacity (L)', required=True
+    )
+    default_tolerance = fields.Float(
+        string='Default Tolerance (%)', required=True
+    )
+
+    active = fields.Boolean(string='Active')
+    description = fields.Text(string='Description or Observations')
+
+    deposit_ids = fields.One2many(
+        'winery.deposit', 
+        'depositType_id', 
+        string="Depósitos reales"
+    )
